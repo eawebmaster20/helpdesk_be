30b8b79dcd0f641f393a8f5f049aca644e24ee97
eawebmaster20 <eaweb20@gmail.com>
2025-09-29 08:00:48 +0000

    feat: implement authentication controller with login, createUser, and updateUser functions

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import {
+  getUsersModel,
+  insertUserModel,
+  updateUserModel,
+} from "../models/users.model";
+import { authenticate } from "../middlewares/authenticate.utils";
+import { verify } from "crypto";
+
+export async function login(req: Request, res: Response) {
+  try {
+    const user = await authenticate(req, res, () => {});
+    console.log(user);
+  } catch (err) {
+    res.status(500).json({ message: "Database error", error: err });
+  }
+}
+
+export async function createUser(req: Request, res: Response) {
+  const { name, email, role, departmentId } = req.body;
+  if (!name || !email || !role) {
+    return res
+      .status(400)
+      .json({ message: "name, email, and role are required" });
+  }
+  try {
+    const result = await insertUserModel(name, email, role, departmentId);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ message: "Database error", error: err });
+  }
+}
+
+export async function updateUser(req: Request, res: Response) {
+  const { id } = req.params;
+  const { name, email, role, departmentId } = req.body;
+  try {
+    const result = await updateUserModel(id, name, email, role, departmentId);
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ message: "Database error", error: err });
+  }
+}
