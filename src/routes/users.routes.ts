f6203ff973e2039adc2d3450b126921cc3f334b9
eawebmaster20 <eaweb20@gmail.com>
2025-10-28 08:24:18 +0000

    feat: add route to retrieve users by group level

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,12 +3,16 @@ import {
   getUsers,
   createUser,
   updateUser,
+  getL2Users,
 } from "../controllers/users.controller";
 
 const router = Router();
 
 // GET /users
 router.get("/", getUsers);
+
+// GET USER GROUP /users/level/:id
+router.get("/level/:id", getL2Users);
 
 // POST /users
 router.post("/", createUser);
